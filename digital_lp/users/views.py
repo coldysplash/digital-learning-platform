b86bb9a5d5e2051344d28a59daa3d04be38756f2
Ivan <ivanreshetnikov235@gmail.com>
2025-05-12 21:32:39 +0700

    update users, add two decorators

--- digital_lp/users/views.py
+++ digital_lp/users/views.py
@@ -3,11 +3,34 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.contrib import messages
+from functools import wraps
+from django.http import Http404
 
 from .forms import UserRegistrationForm, UserLoginForm, UserProfileForm
 from courses.models import Course
 
 
+def is_authenticated_user(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("users:profile")
+        return view_func(request, *args, **kwargs)
+
+    return wrapper
+
+
+def author_required(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_author:
+            raise Http404("You can't create a course, because you're not the author.")
+        return view_func(request, *args, **kwargs)
+
+    return wrapper
+
+
+@is_authenticated_user
 def register_student(request):
     if request.method == "POST":
         form = UserRegistrationForm(request.POST)
@@ -21,6 +44,7 @@ def register_student(request):
     return render(request, "users/register_student.html", {"form": form})
 
 
+@is_authenticated_user
 def register_author(request):
     if request.method == "POST":
         form = UserRegistrationForm(request.POST)
@@ -36,6 +60,7 @@ def register_author(request):
     return render(request, "users/register_author.html", {"form": form})
 
 
+@is_authenticated_user
 def user_login(request):
     if request.method == "POST":
         form = UserLoginForm(data=request.POST)
@@ -52,13 +77,13 @@ def user_login(request):
     return render(request, "users/login.html", {"form": form})
 
 
-@login_required(login_url="/users/login")
+@login_required
 def user_logout(request):
     logout(request)
     return redirect("users:user_login")
 
 
-@login_required(login_url="/users/login")
+@login_required
 def profile(request):
     user = request.user
     if user.is_author:
